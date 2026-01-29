8a44827440ae65dfbe1cf270f93125c78dbd7b7a
Cauã Fernandes <cauapessoapessoa@gmail.com>
2026-01-29 17:18:43 -0300

    Mudança

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,14 +1,19 @@
 package Chess;
 
 import BoardGame.Board;
+import BoardGame.Position;
+import Chess.pieces.King;
+import Chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
-    public ChessMatch(){
-        board = new Board(8,8);
+    public ChessMatch() {
+        board = new Board(8, 8);
+        initialSetup();
     }
-    public ChessPiece[][] getpieces(){
+
+    public ChessPiece[][] getpieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         for (int i = 0; i < board.getRows(); i++) {
             for (int j = 0; j < board.getColumns(); j++) {
@@ -16,5 +21,12 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+
     }
 }
