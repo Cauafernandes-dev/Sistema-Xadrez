d3efaefd2525b0b9e12049564effff8a360c6e36
Cauã Fernandes <cauapessoapessoa@gmail.com>
2026-01-29 19:34:53 -0300

    Mudança

---
+++ src/Chess/ChessException.java
@@ -0,0 +1,9 @@
+package Chess;
+
+public class ChessException extends RuntimeException{
+    private static final long serialVersionUID = 1l;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+}
