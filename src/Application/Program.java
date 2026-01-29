927f3328eb2f777f676086706492409a4bddbb30
Cauã Fernandes <cauapessoapessoa@gmail.com>
2026-01-29 13:42:21 -0300

    Update Program.java

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,7 +1,11 @@
 package Application;
 
+import BoardGame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
