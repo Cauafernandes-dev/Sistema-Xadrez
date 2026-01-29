1995d63a4efda71b1ebfd868b14cad59cceaed67
Cauã Fernandes <cauapessoapessoa@gmail.com>
2026-01-29 16:29:26 -0300

    Update Piece.java

--- src/BoardGame/Piece.java
+++ src/BoardGame/Piece.java
@@ -2,5 +2,15 @@ package BoardGame;
 
 public class Piece {
 
-    protected 
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
 }
