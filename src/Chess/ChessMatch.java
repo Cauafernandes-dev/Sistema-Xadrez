a7328f6f9266da90f7b92a05c3fb3ff0b0db05a6
Cauã Fernandes <cauapessoapessoa@gmail.com>
2026-01-30 15:06:56 -0300

    mudança

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package Chess;
 
 import BoardGame.Board;
-//import BoardGame.Position;
 import BoardGame.Piece;
 import BoardGame.Position;
 import Chess.pieces.King;
@@ -29,6 +28,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validadeTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
 
@@ -47,6 +47,11 @@ public class ChessMatch {
         }
         if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen piece");
+        }
+    }
+    private void validadeTargetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to target position");
         }
     }
 
