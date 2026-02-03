9a169d6f5f5b69b139acb1a75296f0ca31a4cd61
Cauã Fernandes <cauapessoapessoa@gmail.com>
2026-02-03 08:13:24 -0300

    Xadrez

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -6,10 +6,16 @@ import BoardGame.Position;
 import Chess.pieces.King;
 import Chess.pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
-    private final Board board;
-    private int turn;
     private Color currentPlayer;
+    private int turn;
+    private final Board board;
+
+    private List<Piece> pieceOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch() {
         board = new Board(8, 8);
@@ -57,7 +63,13 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+
+        if (capturedPiece != null){
+            pieceOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
+
     }
 
     private void validateSourcePosition(Position position) {
@@ -86,6 +98,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        pieceOnTheBoard.add(piece);
     }
 
     private void initialSetup() {
