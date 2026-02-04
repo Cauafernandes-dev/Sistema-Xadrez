8b0cf490d737b571e60fdf3325bc857aea4dc7e9
Cauã Fernandes <cauapessoapessoa@gmail.com>
2026-02-04 09:01:03 -0300

    Update ChessMatch.java

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -5,9 +5,9 @@ import BoardGame.Piece;
 import BoardGame.Position;
 import Chess.pieces.*;
 
-import java.security.InvalidParameterException;
+
 import java.util.ArrayList;
-import java.util.IllegalFormatCodePointException;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -120,7 +120,7 @@ public class ChessMatch {
 
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
