86fdc6704a0f0aa56db52fd72e0e66b6c08bf2f9
Cauã Fernandes <cauapessoapessoa@gmail.com>
2026-02-05 19:09:16 -0300

    Update UI.java

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -5,37 +5,23 @@ import Chess.ChessPiece;
 import Chess.ChessPosition;
 import Chess.Color;
 
-import java.sql.SQLOutput;
+
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+
 
 public class UI {
 
-    // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
-
-    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
     public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-    public static void clearScreen(){
+
+    public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
@@ -64,10 +50,9 @@ public class UI {
             if (chessMatch.getCheck()) {
                 System.out.println("CHECK!");
             }
-        }
-        else {
+        } else {
             System.out.println("CHECKMATE!");
-            System.out.println("Winner: "+ chessMatch.getCurrentPlayer());
+            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
         }
     }
 
@@ -94,11 +79,11 @@ public class UI {
     }
 
     private static void printPiece(ChessPiece piece, boolean background) {
-        if (background){
+        if (background) {
             System.out.print(ANSI_BLUE_BACKGROUND);
         }
         if (piece == null) {
-            System.out.print("-"+ ANSI_RESET);
+            System.out.print("-" + ANSI_RESET);
         } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
@@ -110,13 +95,12 @@ public class UI {
     }
 
     private static void printCapturedPieces(List<ChessPiece> captured) {
-        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).toList();
         System.out.println("Captured pieces: ");
         // WHITE
         System.out.print("White: ");
         System.out.print(ANSI_WHITE);
-        System.out.println(Arrays.toString(white.toArray()));
+        System.out.println(Arrays.toString(captured.stream().filter(x -> x.getColor() == Color.WHITE).toArray()));
         System.out.print(ANSI_RESET);
         // BLACK
         System.out.print("Black: ");
